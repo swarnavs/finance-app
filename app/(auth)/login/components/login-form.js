4dd4597a057c34f010ee1d0fc5ab2fe09fd031fa
swarnavs <swarnav@capitalnumbers.com>
2025-03-21 14:31:37 +0530

    Modify few file changes

--- app/(auth)/login/components/login-form.js
+++ app/(auth)/login/components/login-form.js
@@ -1,24 +1,34 @@
-'use client'
+"use client";
 import Input from "@/components/input";
 import SubmitButton from "@/components/submit-button";
 import { login } from "@/lib/actions";
-import { useFormState } from "react-dom"
+import { useFormState } from "react-dom";
 
 const initialState = {
-  message: '',
-  error: false
-}
+  message: "",
+  error: false,
+};
 
 export default function LoginForm() {
-  const [state, formAction] = useFormState(login, initialState)
-  return <form action={formAction} className="space-y-2">
-    <Input type="email" placeholder="name@example.com"
-      name="email" required />
-    <SubmitButton type="submit" size="sm" className="w-full">
-      Sign in with email
-    </SubmitButton>
-    <p className={`${state?.error ? 'text-red-500' : 'text-green-500'} text-sm text-center`}>
-      {state?.message}
-    </p>
-  </form>
+  const [state, formAction] = useFormState(login, initialState);
+  return (
+    <form action={formAction} className="space-y-2">
+      <Input
+        type="email"
+        placeholder="name@example.com"
+        name="email"
+        required
+      />
+      <SubmitButton type="submit" size="sm" className="w-full">
+        Sign in with email
+      </SubmitButton>
+      <p
+        className={`${
+          state?.error ? "text-red-500" : "text-green-500"
+        } text-sm text-center`}
+      >
+        {state?.message}
+      </p>
+    </form>
+  );
 }
